9de06544cd7ef5ce43f3bb42cd97191c86d5e412
CChelseaLulu <90858702+CChelseaLulu@users.noreply.github.com>
2021-12-14 22:11:25 +0300

    Q3e

---
+++ q3e.py
@@ -0,0 +1,10 @@
+
+#Modify searchage() such that it gets the search age from the user. 
+def searchage():
+    age = float(input("Enter your age please:"))
+    if age == 5:
+        print(age)
+    else:
+        print("Age is not 5")
+
+searchage()
