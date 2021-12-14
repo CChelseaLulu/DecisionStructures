93e86612a6c11ff62cecb41431291ea0b6eb59a7
CChelseaLulu <90858702+CChelseaLulu@users.noreply.github.com>
2021-12-14 22:11:40 +0300

    Q3f

---
+++ q3f.py
@@ -0,0 +1,18 @@
+def main():
+    infile = open("names.txt", "r")
+def searchname():
+    name = (input("Enter your name please:"))
+    print(name)
+   
+searchname()
+
+def searchage():
+    age = int(input("Enter your age: "))
+    if age != 1 or age != 2:
+        print("Unrealistic! ")
+    exit
+searchage()
+
+main()
+    
+              
